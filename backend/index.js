a1634da3bcfd703bf42166927a6872f7420375a0
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-06-12 23:24:05 -0300

    tentendo colocar online modificação no cors

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const itensRoutes = require('./routes/itensRoutes');
-const UserRoutes = require('./routes/UserRoutes')
+const itensRoutes = require("./routes/itensRoutes");
+const UserRoutes = require("./routes/UserRoutes");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://nome-do-seu-app.onrender.com",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use('/api/itens', itensRoutes);
-app.use('/ImagensVeiculos', express.static(path.join(__dirname, 'banco/ImagensVeiculos')));
-app.use('/banco/ImagensVeiculos', express.static(path.join(__dirname, 'banco/ImagensVeiculos')));
+app.use("/api/itens", itensRoutes);
+app.use(
+  "/ImagensVeiculos",
+  express.static(path.join(__dirname, "banco/ImagensVeiculos"))
+);
+app.use(
+  "/banco/ImagensVeiculos",
+  express.static(path.join(__dirname, "banco/ImagensVeiculos"))
+);
 
-app.use('/api', UserRoutes);
+app.use("/api", UserRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB conectado'))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.error(err));
 
-app.get('/', (req, res) => {
-  res.send('API funcionando!');
+app.get("/", (req, res) => {
+  res.send("API funcionando!");
 });
 
 const PORT = process.env.PORT || 5000;
